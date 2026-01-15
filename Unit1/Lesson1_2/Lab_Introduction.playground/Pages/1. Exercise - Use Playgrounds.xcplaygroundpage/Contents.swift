0093d73b9e521f1b3005b645474a8977d6b03269
celeste <112195258+setselec@users.noreply.github.com>
2026-01-14 21:33:29 -0500

    Completed first lab assignment

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,14 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
-
+print("Limp")
+print("Fiona Apple")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("You want the badge of honor when you save my hide")
+print("But you're the one in the way of the day of doom, baby")
+print("If you need my shame to reclaim your pride")
 /*:
 
  _Copyright © 2023 Apple Inc._
