69672798e5a59e5ea9315e8802e3a662a7ca1772
celeste <112195258+setselec@users.noreply.github.com>
2026-01-19 20:13:41 -0500

    Completed the lab for lesson 1.5

--- Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
@@ -5,11 +5,33 @@
  
  You want your fitness tracking app to give as much encouragement as possible to your users. Create a variable `steps` equal to the number of steps you guess you've taken today. Create a constant `stepGoal` equal to 10,000. Write an if-else statement that will print "You're almost halfway there!" if `steps` is less than half of `stepGoal`, and will print "You're over halfway there!" if `steps` is greater than half of `stepGoal`.
  */
-
-
+let stepGoal = 10000
+var steps = 6000
+let halfOfGoal = stepGoal / 2
+if steps < halfOfGoal {
+    print(
+        "You're almost halfway there!"
+    )
+} else {
+    print (
+        "You're over halfway there!"
+    )
+}
 //:  Now create a new, but similar, if-else-if statement that prints "Way to get a good start today!" if `steps` is less than a tenth of `stepGoal`, prints "You're almost halfway there!" if `steps` is less than half of `stepGoal`, and prints "You're over halfway there!" if `steps` is greater than half of `stepGoal`.
-
-
+let tenthOfGoal = stepGoal / 10
+if steps < tenthOfGoal {
+    print(
+        "Way to get a good start today!"
+    )
+} else if steps < halfOfGoal {
+    print(
+        "You're almost halfway there!"
+    )
+} else {
+    print (
+        "You're over halfway there!"
+    )
+}
 /*:
 [Previous](@previous)  |  page 3 of 9  |  [Next: Exercise - Boolean Practice](@next)
  */
