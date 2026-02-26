23326978c47a99fddd0a83d3bf3834aca31ef0dc
celeste <112195258+setselec@users.noreply.github.com>
2026-02-25 21:03:16 -0500

    Completed Lab: Tutorial Screen

---
+++ Unit2/Lesson2_9/Hobby Tutorial/Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by Celeste Clamage on 2/25/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
